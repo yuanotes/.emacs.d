5ef010a5408fdc25b4e540c8b7c2f81c036cc3bb
Cycreate <yuanotes@gmail.com>
2023-11-02 18:29:41 +0800

    feat: add org-roam package

--- init.el
+++ init.el
@@ -86,5 +86,24 @@
   :diminish nil)
  
 
-
-
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "d:/Projects/orgs/roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (setq org-roam-dailies-directory "日志/")
+  (org-roam-db-autosync-mode))
